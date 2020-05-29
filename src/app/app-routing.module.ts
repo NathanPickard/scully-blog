f19d49d305ea5bfb600008032f0a3f78991903bb
Nathan Pickard <nathan.pickard87@gmail.com>
2020-05-29 16:37:43 -0700

    Configured the home module as project root and injected scully routes service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
